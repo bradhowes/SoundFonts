03cc9672f506c93416597e2ffeb90443570d8aa8
Brad Howes <bradhowes@mac.com>
2020-01-04 13:03:22 +0100

    Fixed crash when editing favorite.

--- SoundFontsUITests/FastlaneSnapshots.swift
+++ SoundFontsUITests/FastlaneSnapshots.swift
@@ -4,9 +4,21 @@ import XCTest
 
 class FastlaneSnapshots: XCTestCase {
 
+    var switcher: XCUIElement!
+
     override func setUp() {
         continueAfterFailure = false
         let app = XCUIApplication()
+        switcher = app.children(matching: .window)
+            .element(boundBy: 0)
+            .children(matching: .other)
+            .element.children(matching: .other)
+            .element(boundBy: 2)
+            .children(matching: .other)
+            .element.children(matching: .other)
+            .element.children(matching: .other)
+            .element
+
         setupSnapshot(app)
         app.launch()
     }
@@ -15,16 +27,30 @@ class FastlaneSnapshots: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func showSoundFontView() {
+        let app = XCUIApplication()
+        let e1: XCUIElementQuery = app.children(matching: .window)
+        guard e1.count > 0 else { return }
+        let e2 = e1.element(boundBy: 0)
+        let e3 = e2.children(matching: .other).element
+        let e4 = e3.children(matching: .other)
+        guard e4.count > 1 else { return }
+        let e5 = e4.element(boundBy: 1).children(matching: .other).element.children(matching: .collectionView)
+        guard e5.count > 0 else { return }
+        let e6 = e5.element
+        e6.swipeRight()
+    }
+
     func switchViews() {
-        XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element(boundBy: 2).children(matching: .other).element.children(matching: .other).element.swipeLeft()
+        switcher.doubleTap()
     }
 
     func testPortrait() {
         XCUIDevice.shared.orientation = .portrait
+        showSoundFontView()
         snapshot("0FontsPortrait")
         switchViews()
         snapshot("0FavesPortrait")
-        switchViews()
     }
 
     func testLandscape() {
