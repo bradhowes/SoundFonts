e5a337126406c58a0d34bc14ea8764c4578411c6
Brad Howes <bradhowes@mac.com>
2020-02-08 14:25:18 +0100

    Renamed directories for clarity

--- SoundFontAU/SoundFontAU-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  SoundFontAU-Bridging-Header.h
-//  SoundFontAU
-//
-//  Created by Brad Howes on 1/31/20.
-//  Copyright © 2020 Brad Howes. All rights reserved.
-//
-
-#import "SoundFontAUDSPKernelAdapter.h"
