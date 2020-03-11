7b415486cf3f51e566b4d0c56016a4a2ec118ae7
Brad Howes <bradhowes@mac.com>
2020-03-11 23:34:57 +0100

    Cleanup

--- SoundFontsFramework/Notifiication.swift
+++ SoundFontsFramework/Notifiication.swift
@@ -5,11 +5,11 @@ import UIKit
 extension Notification {
 
     /// Notification to check if now is a good time to ask for a review of the app
-    static let askForReview = Notification(name: Notification.Name(rawValue: "askForReview"))
+    public static let askForReview = Notification(name: Notification.Name(rawValue: "askForReview"))
 
     /// Notification that the `showKeyLabels` setting changed
-    static let showKeyLabelsChanged = Notification(name: Notification.Name(rawValue: "showKeyLabelsChanged"))
+    public static let showKeyLabelsChanged = Notification(name: Notification.Name(rawValue: "showKeyLabelsChanged"))
 
     /// Notification to visit the app store to review the app
-    static let visitAppStore = Notification(name: Notification.Name(rawValue: "visitAppStore"))
+    public static let visitAppStore = Notification(name: Notification.Name(rawValue: "visitAppStore"))
 }
