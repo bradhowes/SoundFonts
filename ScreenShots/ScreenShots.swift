01e663948d88530feade3ddf263a195fdccdcd03
Brad Howes <bradhowes@mac.com>
2020-03-12 10:50:58 +0100

    Improved keyboard sizing on iPad devices
    Always show 'more' buttons when wide enough
    Really fix incorrect selectedIndicator settings

--- ScreenShots/ScreenShots.swift
+++ ScreenShots/ScreenShots.swift
@@ -1,78 +1,37 @@
-// Copyright © 2019 Brad Howes. All rights reserved.
+//
+//  ScreenShots.swift
+//  ScreenShots
+//
+//  Created by Brad Howes on 3/12/20.
+//  Copyright © 2020 Brad Howes. All rights reserved.
+//
 
 import XCTest
+import SoundFontsFramework
 
 class ScreenShots: XCTestCase {
 
-    var app: XCUIApplication!
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
 
-    func __testTest() {
-        let mainView = app.otherElements["MainView"]
-        XCTAssert(mainView.waitForExistence(timeout: 5))
-
-        let soundFontsView = app.otherElements["SoundFontsView"]
-        XCTAssert(soundFontsView.exists)
-
-        let favoritesView = app.otherElements["FavoritesView"]
-        XCTAssert(!favoritesView.exists)
-
-        let touchView = app.otherElements["TouchView"]
-        XCTAssert(touchView.exists)
-
-        touchView.doubleTap()
-        XCTAssert(favoritesView.waitForExistence(timeout: 5))
-    }
-
-    func switchViews() {
-        let touchView = app.otherElements["TouchView"]
-        touchView.doubleTap()
-    }
-
-    func showSoundFontsView() {
-
-        // Make sure that the SoundFontsView is visible
-        let soundFontsView = app.otherElements["SoundFontsView"]
-        guard !soundFontsView.exists else { return }
-        switchViews()
-        XCTAssert(soundFontsView.waitForExistence(timeout: 5))
-    }
-
-    func showFavoritesView() {
-
-        // Make sure that the FavoritesView is visible
-        let favoritesView = app.otherElements["FavoritesView"]
-        guard !favoritesView.exists else { return }
-        switchViews()
-        XCTAssert(favoritesView.waitForExistence(timeout: 5))
-    }
-
-    func initialize(_ orientation: UIDeviceOrientation) {
-
-        // Do this before creating the app
-        XCUIDevice.shared.orientation = orientation
+        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // Create the app but wait until the MainView is visible
-        app = XCUIApplication()
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
-        let mainView = app.otherElements["MainView"]
-        XCTAssert(mainView.waitForExistence(timeout: 5))
-    }
 
-    func testPortrait() {
-        initialize(.portrait)
-        showSoundFontsView()
-        snapshot("Patches-Portrait")
-        showFavoritesView()
-        snapshot("Favorites-Portrait")
-    }
-
-    func testLandscape() {
-        initialize(.landscapeLeft)
-        showSoundFontsView()
-        snapshot("Patches-Landscape")
-        showFavoritesView()
-        snapshot("Favorites-Landscape")
+        snapshot("01MainScreen")
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 }
