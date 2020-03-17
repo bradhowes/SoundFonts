e8152f3739c980c6f699d006d94b28d401528759
Brad Howes <bradhowes@mac.com>
2020-03-17 16:59:08 +0100

    Show keyboard while changing key width

--- SoundFontsFramework/SettingsViewController.swift
+++ SoundFontsFramework/SettingsViewController.swift
@@ -18,14 +18,23 @@ public final class SettingsViewController: UIViewController {
 
     @IBOutlet private weak var contentView: UIView!
     @IBOutlet private weak var doneButton: UIBarButtonItem!
-
     @IBOutlet private weak var showSolfegeNotes: UISwitch!
     @IBOutlet private weak var playSample: UISwitch!
     @IBOutlet private weak var showKeyLabels: UISwitch!
-    @IBOutlet private weak var keyWidthSlider: UISlider!
 
+    @IBOutlet private weak var keyWidthSlider: UISlider!
+    @IBOutlet weak var keyWidthLabel: UILabel!
+
+    @IBOutlet weak var removeDefaultSoundFontsLabel: UILabel!
     @IBOutlet private weak var removeDefaultSoundFonts: UIButton!
+
+    @IBOutlet weak var restoreDefaultSoundFontsLabel: UILabel!
     @IBOutlet private weak var restoreDefaultSoundFonts: UIButton!
+
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var review: UIButton!
+
+    @IBOutlet weak var lowerContent: UIView!
 
     var soundFonts: SoundFonts!
 
@@ -42,9 +51,49 @@ public final class SettingsViewController: UIViewController {
         keyWidthSlider.isContinuous = true
         keyWidthSlider.value = Settings[.keyWidth]
 
+        endShowKeyboard()
         preferredContentSize = contentView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
     }
 
+    private func beginShowKeyboard() {
+        let newColor = view.backgroundColor?.withAlphaComponent(0.2)
+        view.backgroundColor = newColor
+        lowerContent.alpha = 0.0
+        removeDefaultSoundFontsLabel.alpha = 0.0
+        removeDefaultSoundFonts.alpha = 0.0
+        restoreDefaultSoundFontsLabel.alpha = 0.0
+        restoreDefaultSoundFonts.alpha = 0.0
+        versionLabel.alpha = 0.0
+        review.alpha = 0.0
+        keyWidthLabel.alpha = 0.0
+    }
+
+    private func endShowKeyboard() {
+        let newColor = view.backgroundColor?.withAlphaComponent(1.0)
+        view.backgroundColor = newColor
+        lowerContent.alpha = 1.0
+        removeDefaultSoundFontsLabel.alpha = 1.0
+        removeDefaultSoundFonts.alpha = 1.0
+        restoreDefaultSoundFontsLabel.alpha = 1.0
+        restoreDefaultSoundFonts.alpha = 1.0
+        versionLabel.alpha = 1.0
+        review.alpha = 1.0
+        keyWidthLabel.alpha = 1.0
+    }
+
+    @IBAction func keyWidthEditingDidBegin(_ sender: Any) {
+        os_log(.info, log: log, "keyWidthEditingDidBegin")
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0.0, options: [.allowUserInteraction],
+                                                       animations: self.beginShowKeyboard,
+                                                       completion: { _ in self.beginShowKeyboard() })
+    }
+
+    @IBAction func keyWidthEditingDidEnd(_ sender: Any) {
+        os_log(.info, log: log, "keyWidthEditingDidEnd")
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0.0, options: [.allowUserInteraction],
+                                                       animations: self.endShowKeyboard,
+                                                       completion: { _ in self.endShowKeyboard() })
+    }
     private func updateButtonState() {
         restoreDefaultSoundFonts.isEnabled = !soundFonts.hasAllBundled
         removeDefaultSoundFonts.isEnabled = soundFonts.hasAnyBundled
