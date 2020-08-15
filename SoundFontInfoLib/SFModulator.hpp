38788f3d2fbd570c008617ba0857422aac4ba77d
Brad Howes <bradhowes@mac.com>
2020-08-15 12:33:11 +0200

    Renaming for better understanding

--- SoundFontInfoLib/SFModulator.hpp
+++ SoundFontInfoLib/SFModulator.hpp
@@ -10,15 +10,22 @@ namespace SF2 {
 struct SFModulator {
     static constexpr char const* typeNames[] = { "linear", "concave", "convex", "switch" };
 
-    SFModulator() : bits_{0} {}
-    SFModulator(uint16_t bits) : bits_{bits} {}
+    constexpr SFModulator() : bits_{0} {}
+    constexpr SFModulator(uint16_t bits) : bits_{bits} {}
 
     short type() const { return bits_ >> 10; }
-    bool polarity() const { return bits_ & (1 << 9); }
-    bool direction() const { return bits_ & (1 << 8); }
-    bool continuousController() const { return bits_ & (1 << 7); }
-    short index() const { return bits_ & 0x7F; }
 
+    short polarity() const { return bits_ & (1 << 9); }
+    bool isUnipolar() const { return polarity() == 0; }
+    bool isBipolar() const { return !isUnipolar(); }
+
+    bool direction() const { return bits_ & (1 << 8); }
+    bool isMinToMax() const { return direction() == 0; }
+    bool isMaxToMin() const { return !isMinToMax(); }
+
+    bool isContinuousController() const { return (bits_ & (1 << 7)) ? true : false; }
+
+    short index() const { return bits_ & 0x7F; }
     char const* typeName() const { return typeNames[type()]; }
 
     friend std::ostream& operator<<(std::ostream& os, SFModulator const& mod)
@@ -26,13 +33,13 @@ struct SFModulator {
         return os << "[type: " << mod.typeName()
         << " P: " << mod.polarity()
         << " D: " << mod.direction()
-        << " CC: " << mod.continuousController()
+        << " CC: " << mod.isContinuousController()
         << " index: " << mod.index()
         << "]";
     }
 
 private:
-    const uint16_t bits_;
+    uint16_t bits_;
 };
 
 }
