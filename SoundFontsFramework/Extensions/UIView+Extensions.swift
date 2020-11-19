dab370f3ce25427657ddde448897edad6d9760cf
Brad Howes <bradhowes@mac.com>
2020-11-19 17:40:58 +0100

    Refactor label extension

--- SoundFontsFramework/Extensions/UIView+Extensions.swift
+++ SoundFontsFramework/Extensions/UIView+Extensions.swift
@@ -22,32 +22,3 @@ public extension UIView {
         set { layer.borderColor =  newValue?.cgColor }
     }
 }
-
-public extension UILabel {
-
-    func showStatus(_ status: String, duration: Double = 1.0) {
-        if let statusTimer = (objc_getAssociatedObject(self, "statusTimer") as? Timer) {
-            statusTimer.invalidate()
-        }
-
-        let originalText = (objc_getAssociatedObject(self, "originalText") as? String) ?? self.text
-        objc_setAssociatedObject(self, "originalText", originalText, .OBJC_ASSOCIATION_RETAIN)
-        let statusTimer = Timer.once(after: duration) { _ in
-            self.fadeTransition(0.4)
-            self.text = originalText
-        }
-
-        self.layer.removeAllAnimations()
-        objc_setAssociatedObject(self, "statusTimer", statusTimer, .OBJC_ASSOCIATION_RETAIN)
-        fadeTransition(0.4)
-        self.text = status
-    }
-
-    private func fadeTransition(_ duration: CFTimeInterval) {
-        let animation = CATransition()
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-        animation.type = CATransitionType.fade
-        animation.duration = duration
-        layer.add(animation, forKey: CATransitionType.fade.rawValue)
-    }
-}
