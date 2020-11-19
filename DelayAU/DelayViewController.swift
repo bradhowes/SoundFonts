5f312dfaf4ecb801e7fcaa7b187a9ad16125d2cc
Brad Howes <bradhowes@mac.com>
2020-11-19 09:28:35 +0100

    Add controls to delay and reverb extensions

--- DelayAU/DelayViewController.swift
+++ DelayAU/DelayViewController.swift
@@ -13,11 +13,14 @@ public final class DelayViewController: AUViewController {
     @IBOutlet weak var enabledButton: UIButton!
     @IBOutlet weak var controls: UIStackView!
     @IBOutlet weak var time: Knob!
+    @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var feedback: Knob!
+    @IBOutlet weak var feedbackLabel: UILabel!
     @IBOutlet weak var cutoff: Knob!
+    @IBOutlet weak var cutoffLabel: UILabel!
     @IBOutlet weak var wetDryMix: Knob!
-    @IBOutlet weak var valueLabel: UILabel!
-    
+    @IBOutlet weak var wetDryMixLabel: UILabel!
+
     public override func viewDidLoad() {
         os_log(.info, log: log, "viewDidLoad")
         super.viewDidLoad()
@@ -26,31 +29,40 @@ public final class DelayViewController: AUViewController {
         feedback.value = settings.delayFeedback
         cutoff.value = settings.delayCutoff
         wetDryMix.value = settings.delayWetDryMix
-        updateDelayState(settings.delayEnabled)
+        updateEnabledState(true)
     }
 
-    @IBAction func toggleDelayEnabled(_ sender: UIButton) {
+    @IBAction func toggleEnabled(_ sender: UIButton) {
         let enabled = !wetDryMix.isEnabled
-        updateDelayState(enabled)
+        updateEnabledState(enabled)
     }
 
-    @IBAction func changeDelayTime(_ sender: Any) {
+    @IBAction func changeTime(_ sender: Any) {
         let value = time.value
+        timeLabel.showStatus(String(format: "%.2f", value) + "s")
         settings.delayTime = value
     }
 
-    @IBAction func changeDelayFeedback(_ sender: Any) {
+    @IBAction func changeFeedback(_ sender: Any) {
         let value = feedback.value
+        feedbackLabel.showStatus(String(format: "%.0f", value) + "%")
         settings.delayFeedback = value
     }
 
-    @IBAction func changeDelayCutoff(_ sender: Any) {
+    @IBAction func changeCutoff(_ sender: Any) {
         let value = cutoff.value
+        if value < 1000.0 {
+            cutoffLabel.showStatus(String(format: "%.1f", value) + " Hz")
+        }
+        else {
+            cutoffLabel.showStatus(String(format: "%.2f", value / 1000.0) + " kHz")
+        }
         settings.delayCutoff = value
     }
 
-    @IBAction func changeDelayWebDryMix(_ sender: Any) {
+    @IBAction func changeWebDryMix(_ sender: Any) {
         let value = wetDryMix.value
+        wetDryMixLabel.showStatus(String(format: "%.0f", value) + "%")
         settings.delayWetDryMix = value
     }
 }
@@ -59,7 +71,7 @@ extension DelayViewController: AUAudioUnitFactory {
 
     public func createAudioUnit(with componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
         let audioUnit = try DelayAU(componentDescription: componentDescription, delay: delay)
-        os_log(.info ,log: log, "created ReverbAU")
+        os_log(.info ,log: log, "created DelayAU")
         self.audioUnit = audioUnit
         return audioUnit
     }
@@ -67,13 +79,24 @@ extension DelayViewController: AUAudioUnitFactory {
 
 extension DelayViewController {
 
-    private func updateDelayState(_ enabled: Bool) {
+    private func updateEnabledState(_ enabled: Bool) {
         enabledLED.backgroundColor = enabled ? .systemGreen  : .darkGray
         controls.alpha = enabled ? 1.0 : 0.5
-        wetDryMix.isEnabled = enabled
         time.isEnabled = enabled
         feedback.isEnabled = enabled
         cutoff.isEnabled = enabled
+        wetDryMix.isEnabled = enabled
         settings.delayEnabled = enabled
+    }
+}
+
+extension UIView {
+
+    func fadeTransition(duration: CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        animation.type = .fade
+        animation.duration = duration
+        layer.add(animation, forKey: CATransitionType.fade.rawValue)
     }
 }
