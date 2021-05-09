d06b775f772a21342547683d3ee91aaf28b68d37
Brad Howes <bradhowes@mac.com>
2021-05-09 22:40:52 +0200

    Cleanup

--- SoundFontInfoLib/Render/Engine.hpp
+++ SoundFontInfoLib/Render/Engine.hpp
@@ -12,21 +12,15 @@
 namespace SF2 {
 namespace Render {
 
-template <typename T>
 class Engine : public T {
 public:
 
-    Engine(double sampleRate) : T(), sampleRate_{sampleRate} {}
-
-    void load(const IO::File& file) { T::load(file); };
+    Engine(double sampleRate);
 
     const Preset& preset(size_t index) const;
-    const Preset* lookup(UByte bank, UByte preset) const;
+    const Preset* lookup(short bank, short preset) const;
 
 private:
-    std::vector<Preset> presets_;
-    std::vector<Instrument> instruments_;
-    std::vector<Sample::CanonicalBuffer<AUValue>> sampleBuffers_;
 };
 
 } // namespace Render
