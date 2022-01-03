87a6ace93946d2286f00bd3828f2a669d65ac190
Brad Howes <bradhowes@mac.com>
2022-01-03 12:12:43 +0100

    Updated tests.

--- SoundFontsFramework/SoundFontsFrameworkTests/SelectedSoundFontManagerTests.swift
+++ SoundFontsFramework/SoundFontsFrameworkTests/SelectedSoundFontManagerTests.swift
@@ -1,8 +1,52 @@
 // Copyright © 2020 Brad Howes. All rights reserved.
 
-import SoundFontsFramework
+@testable import SoundFontsFramework
 import SoundFontInfoLib
 import XCTest
+
+class SelectedSoundFontManagerTests: XCTestCase {
+
+  let soundFontInfo = SoundFontInfo(
+    "name",
+    url: URL(fileURLWithPath: "/a/b/c"),
+    author: "author",
+    comment: "comment",
+    copyright: "copyright",
+    presets: [
+      SoundFontInfoPreset("a", bank: 1, preset: 1),
+      SoundFontInfoPreset("b", bank: 1, preset: 2)
+    ]
+  )
+
+  lazy var soundFont = SoundFont("name", soundFontInfo: soundFontInfo!, resource: URL(fileURLWithPath: "a/b/c"))
+
+  func testSetting() {
+    let manager = SelectedSoundFontManager()
+
+    let monitor = Monitor()
+    monitor.token = manager.subscribe(monitor, notifier: monitor.closure)
+
+    monitor.makeExpectation()
+    manager.setSelected(soundFont)
+
+    // Repeated setSelected with same font does not fire
+    monitor.makeExpectation(inverted: true)
+    manager.setSelected(soundFont)
+
+    monitor.makeExpectation()
+    manager.clearSelected()
+
+    // Repeated clearSelected does not fire
+    monitor.makeExpectation(inverted: true)
+    manager.clearSelected()
+
+    monitor.makeExpectation()
+    manager.setSelected(soundFont)
+
+    wait(for: monitor.expectations, timeout: 0.1)
+    XCTAssertEqual(monitor.counter, 3)
+  }
+}
 
 private class Monitor {
   var token: SubscriberToken?
@@ -26,40 +70,4 @@ private class Monitor {
   }
 }
 
-class SelectedSoundFontManagerTests: XCTestCase {
 
-  func testSetting() {
-    let soundFontInfo = SoundFontInfo(
-      "name",
-      url: URL(fileURLWithPath: "/a/b/c"),
-      author: "author",
-      comment: "comment",
-      copyright: "copyright",
-      presets: [
-        SoundFontInfoPreset("a", bank: 1, preset: 1),
-        SoundFontInfoPreset("b", bank: 1, preset: 2)
-      ]
-    )
-
-    let soundFont = SoundFont("name", soundFontInfo: soundFontInfo!, resource: URL(fileURLWithPath: "a/b/c"))
-    let manager = SelectedSoundFontManager()
-
-    let monitor = Monitor()
-    monitor.token = manager.subscribe(monitor, notifier: monitor.closure)
-
-    monitor.makeExpectation()
-    manager.setSelected(soundFont)
-
-    monitor.makeExpectation(inverted: true)
-    manager.setSelected(soundFont)
-
-    monitor.makeExpectation()
-    manager.clearSelected()
-
-    monitor.makeExpectation()
-    manager.setSelected(soundFont)
-
-    wait(for: monitor.expectations, timeout: 0.1)
-    XCTAssertEqual(monitor.counter, 3)
-  }
-}
