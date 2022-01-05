d0f7b5de96d23b346c7d45e5f71cf514bfbb6087
Brad Howes <bradhowes@mac.com>
2022-01-05 16:57:25 +0100

    Add convenience method.

--- SoundFontsFramework/SoundFontsFramework/Extensions/DispatchTime.swift
+++ SoundFontsFramework/SoundFontsFramework/Extensions/DispatchTime.swift
@@ -1,5 +1,6 @@
 import Foundation
 
-extension DispatchTime {
+public extension DispatchTime {
   static func future(_ delta: DispatchTimeInterval) -> DispatchTime { DispatchTime.now() + delta }
+  static func future(_ delta: Double) -> DispatchTime { DispatchTime.now() + delta }
 }
