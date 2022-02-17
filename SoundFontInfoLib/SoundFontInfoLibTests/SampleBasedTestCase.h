ec1a7a9f4fd5e0202b5c3fc81022a2f6aa9334d1
Brad Howes <bradhowes@mac.com>
2022-02-17 19:44:54 +0100

    Fixed LFO testing

--- SoundFontInfoLib/SoundFontInfoLibTests/SampleBasedTestCase.h
+++ SoundFontInfoLib/SoundFontInfoLibTests/SampleBasedTestCase.h
@@ -1,64 +1,63 @@
-//// Copyright © 2021 Brad Howes. All rights reserved.
-////
+// Copyright © 2021 Brad Howes. All rights reserved.
 //
-//#pragma once
-//
-//#import <XCTest/XCTest.h>
-//
-//#import "IO/File.hpp"
-//#import "Render/Preset.hpp"
-//
-//struct PresetTestContextBase
-//{
-//  inline static double epsilon = 0.0001;
-//  static SF2::IO::File makeFile(int urlIndex);
-//};
-//
-//template <int UrlIndex>
-//struct PresetTestContext : PresetTestContextBase
-//{
-//  PresetTestContext() :
-//  file_{makeFile(UrlIndex)},
-//  instruments_{file_},
-//  preset_{file_, instruments_, file_.presets()[0]},
-//  channel_{}
-//  {}
-//
-//  const SF2::IO::File& file() const { return file_; }
-//
-//  const SF2::Render::Preset& preset() const { return preset_; }
-//
-//  SF2::Render::Preset::VoiceConfigCollection find(int key, int velocity) const { return preset_.find(key, velocity); }
-//
-//  SF2::Render::Voice::State makeState(int key, int velocity) const {
-//    return SF2::Render::Voice::State(44100, channel_, find(key, velocity)[0]);
-//  }
-//
-//  SF2::Render::Voice::State makeState(const SF2::Render::Voice::Config& config, double sampleRate = 44100) const {
-//    return SF2::Render::Voice::State(sampleRate, channel_, config);
-//  }
-//
-//  SF2::MIDI::Channel& channel() { return channel_; }
-//
-//  static void SamplesEqual(double a, double b) {
-//    XCTAssertEqualWithAccuracy(a, b, epsilon);
-//  }
-//
-//private:
-//  SF2::IO::File file_;
-//  SF2::Render::InstrumentCollection instruments_;
-//  SF2::Render::Preset preset_;
-//  SF2::MIDI::Channel channel_;
-//};
-//
-//@interface SampleBasedTestCase : XCTestCase {
-//  PresetTestContextBase context;
-//  PresetTestContext<0> context0;
-//  PresetTestContext<1> context1;
-//  PresetTestContext<2> context2;
-//  PresetTestContext<3> context3;
-//}
-//
-//- (void)sample:(double)A equals:(double)B;
-//
-//@end
+
+#pragma once
+
+#import <XCTest/XCTest.h>
+
+#import "IO/File.hpp"
+#import "Render/Preset.hpp"
+#import "Render/State.hpp"
+
+struct PresetTestContextBase
+{
+  inline static double epsilon = 0.0001;
+  static SF2::IO::File makeFile(int urlIndex);
+};
+
+template <int UrlIndex>
+struct PresetTestContext : PresetTestContextBase
+{
+  PresetTestContext() :
+  file_{makeFile(UrlIndex)},
+  instruments_{file_},
+  preset_{file_, instruments_, file_.presets()[0]},
+  channel_{}
+  {}
+
+  const SF2::IO::File& file() const { return file_; }
+
+  const SF2::Render::Preset& preset() const { return preset_; }
+
+  SF2::Render::Preset::ConfigCollection find(int key, int velocity) const { return preset_.find(key, velocity); }
+
+  SF2::Render::State makeState(const SF2::Render::Config& config, double sampleRate = 44100) const {
+    SF2::Render::State state(sampleRate, channel_);
+    config.apply(state);
+    return state;
+  }
+
+  SF2::MIDI::Channel& channel() { return channel_; }
+
+  static void SamplesEqual(double a, double b) {
+    XCTAssertEqualWithAccuracy(a, b, epsilon);
+  }
+
+private:
+  SF2::IO::File file_;
+  SF2::Render::InstrumentCollection instruments_;
+  SF2::Render::Preset preset_;
+  SF2::MIDI::Channel channel_;
+};
+
+@interface SampleBasedTestCase : XCTestCase {
+  PresetTestContextBase context;
+  PresetTestContext<0> context0;
+  PresetTestContext<1> context1;
+  PresetTestContext<2> context2;
+  PresetTestContext<3> context3;
+}
+
+- (void)sample:(double)A equals:(double)B;
+
+@end
--- SoundFontInfoLib/SoundFontInfoLibTests/SampleBasedTestCase.mm
+++ SoundFontInfoLib/SoundFontInfoLibTests/SampleBasedTestCase.mm
@@ -1,26 +1,26 @@
-//// Copyright © 2021 Brad Howes. All rights reserved.
-////
+// Copyright © 2021 Brad Howes. All rights reserved.
 //
-//#import <SF2Files/SF2Files-Swift.h>
-//
-//#import "SampleBasedTestCase.h"
-//
-//using namespace SF2;
-//using namespace SF2::Render;
-//
-//SF2::IO::File PresetTestContextBase::makeFile(int urlIndex)
-//{
-//  NSArray<NSURL*>* urls = SF2Files.allResources;
-//  NSURL* url = [urls objectAtIndex:urlIndex];
-//  uint64_t fileSize = [[[NSFileManager defaultManager] attributesOfItemAtPath:url.path error:nil] fileSize];
-//  int fd = ::open(url.path.UTF8String, O_RDONLY);
-//  return IO::File(fd, fileSize);
-//}
-//
-//@implementation SampleBasedTestCase
-//
-//- (void)sample:(double)A equals:(double)B {
-//  XCTAssertEqualWithAccuracy(A, B, context0.epsilon);
-//}
-//
-//@end
+
+#import <SF2Files/SF2Files-Swift.h>
+
+#import "SampleBasedTestCase.h"
+
+using namespace SF2;
+using namespace SF2::Render;
+
+SF2::IO::File PresetTestContextBase::makeFile(int urlIndex)
+{
+  NSArray<NSURL*>* urls = SF2Files.allResources;
+  NSURL* url = [urls objectAtIndex:urlIndex];
+  uint64_t fileSize = [[[NSFileManager defaultManager] attributesOfItemAtPath:url.path error:nil] fileSize];
+  int fd = ::open(url.path.UTF8String, O_RDONLY);
+  return IO::File(fd, fileSize);
+}
+
+@implementation SampleBasedTestCase
+
+- (void)sample:(double)A equals:(double)B {
+  XCTAssertEqualWithAccuracy(A, B, context0.epsilon);
+}
+
+@end
