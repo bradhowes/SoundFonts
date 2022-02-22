cc2b187b78fdd05941d12e3c76ebd93177233d85
Brad Howes <bradhowes@mac.com>
2022-02-22 16:23:14 +0100

    Commentary and access to name.

--- SoundFontInfoLib/SoundFontInfoLib/Entity/SampleHeader.hpp
+++ SoundFontInfoLib/SoundFontInfoLib/Entity/SampleHeader.hpp
@@ -15,6 +15,17 @@ namespace SF2::Entity {
  to alignment/padding the struct below is 48 bytes.
  
  The offsets (begin, end, loopBegin, and loopEnd) are indices into a big array of 16-bit integer sample values.
+
+ From the SF2 spec:
+
+ The values of dwStart, dwEnd, dwStartloop, and dwEndloop must all be within the range of the sample data field
+ included in the SoundFont compatible bank or referenced in the sound ROM. Also, to allow a variety of hardware
+ platforms to be able to reproduce the data, the samples have a minimum length of 48 data points, a minimum loop size
+ of 32 data points and a minimum of 8 valid points prior to dwStartloop and after dwEndloop. Thus dwStart must be less
+ than dwStartloop-7,
+
+ dwStartloop must be less than dwEndloop-31, and dwEndloop must be less than dwEnd-7. If these constraints are not met,
+ the sound may optionally not be played if the hardware cannot support artifact-free playback for the parameters given.
  */
 class SampleHeader {
 public:
@@ -44,9 +55,9 @@ public:
    Construct instance for unit tests.
    */
   SampleHeader(uint32_t start, uint32_t end, uint32_t loopBegin, uint32_t loopEnd,
-               uint32_t sampleRate, uint8_t key, int8_t adjustment)
-  : dwStart{start}, dwEnd{end}, dwStartLoop{loopBegin}, dwEndLoop{loopEnd}, dwSampleRate{sampleRate},
-  originalKey{key}, correction{adjustment} {}
+               uint32_t sampleRate, uint8_t key, int8_t adjustment) :
+  dwStart{start}, dwEnd{end}, dwStartLoop{loopBegin}, dwEndLoop{loopEnd}, dwSampleRate{sampleRate}, originalKey{key},
+  correction{adjustment} {}
   
   /// @returns true if this sample only has one channel
   bool isMono() const { return (sampleType & monoSample) == monoSample; }
@@ -59,18 +70,43 @@ public:
   
   /// @returns true if samples come from a ROM
   bool isROM() const { return (sampleType & rom) == rom; }
-  
-  /// @returns the index of the first sample to use
+
+  const char* sampleName() const { return achSampleName; }
+
+  bool hasLoop() const { return dwStartLoop > dwStart && dwStartLoop < dwEndLoop && dwEndLoop <= dwEnd; }
+
+  /**
+   The DWORD dwStart contains the index, in sample data points, from the beginning of the sample data field to the
+   first data point of this sample.
+
+   @returns the index of the first sample to use
+   */
   size_t startIndex() const { return dwStart; }
   
-  /// @returns index + 1 of the last sample to use. According to spec, this is first of 46 0.0 values after the last
-  /// sample
+  /**
+   The DWORD dwEnd contains the index, in sample data points, from the beginning of the sample data field to the first
+   of the set of 46 zero valued data points following this sample.
+
+   @returns index + 1 of the last sample to use.
+   */
   size_t endIndex() const { return dwEnd; }
-  
-  /// @returns index of the first sample in a loop.
+
+  /**
+   The DWORD dwStartloop contains the index, in sample data points, from the beginning of the sample data field to the
+   first data point in the loop of this sample.
+
+   @returns index of the first sample in a loop.
+   */
   size_t startLoopIndex() const { return dwStartLoop; }
-  
-  /// @returns index of the last + 1 of a sample in a loop.
+
+  /**
+   The DWORD dwEndloop contains the index, in sample data points, from the beginning of the sample data field to the
+   first data point following the loop of this sample. Note that this is the data point “equivalent to” the first loop
+   data point, and that to produce portable artifact free loops, the eight proximal data points surrounding both the
+   Startloop and Endloop points should be identical.
+
+   @returns index of the last + 1 of a sample in a loop.
+   */
   size_t endLoopIndex() const { return dwEndLoop; }
   
   /// @returns the sample rate used to record the samples in the SF2 file
