9224f44ccf5833b3e25d19b6cf1b853f5937424a
Brad Howes <bradhowes@mac.com>
2022-02-26 16:31:58 +0100

    Use XCTest functions.

--- SoundFontInfoLib/SoundFontInfoLibTests/Render/LFOTests.mm
+++ SoundFontInfoLib/SoundFontInfoLibTests/Render/LFOTests.mm
@@ -19,7 +19,12 @@ struct LFOTestInjector {
 @end
 
 @implementation LFOTests {
-SampleBasedContexts contexts;
+  SampleBasedContexts contexts;
+  SF2::Float epsilon;
+}
+
+- (void)setUp {
+  epsilon = 1.0e-8f;
 }
 
 - (void)tearDown {
@@ -28,49 +33,49 @@ SampleBasedContexts contexts;
 
 - (void)testSamples {
   auto osc = LFOTestInjector::make(8.0, 1.0, 0.0);
-  [self sample:osc.value() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.value() equals:0.5];
-  [self sample:osc.value() equals:0.5];
-  [self sample:osc.getNextValue() equals:0.5];
-  [self sample:osc.value() equals:1.0];
-  [self sample:osc.value() equals:1.0];
-  [self sample:osc.getNextValue() equals:1.0];
-  [self sample:osc.getNextValue() equals:0.5];
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:-0.5];
-  [self sample:osc.getNextValue() equals:-1.0];
-  [self sample:osc.getNextValue() equals:-0.5];
-  [self sample:osc.getNextValue() equals:0.0];
+  XCTAssertEqualWithAccuracy(osc.value(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.value(), 0.5, epsilon);
+  XCTAssertEqualWithAccuracy(osc.value(), 0.5, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.5, epsilon);
+  XCTAssertEqualWithAccuracy(osc.value(), 1.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.value(), 1.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 1.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.5, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), -0.5, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), -1.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), -0.5, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
 }
 
 - (void)testDelay {
   auto osc = LFOTestInjector::make(8.0, 1.0, 0.125);
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.5];
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.5, epsilon);
   osc = LFOTestInjector::make(8.0, 1.0, 0.25);
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.5];
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.5, epsilon);
 }
 
 - (void)testConfig {
   auto osc = LFOTestInjector::make(8.0, 1.0, 0.125);
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.5];
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.5, epsilon);
   osc = LFOTestInjector::make(8.0, 1.0, 0.0);
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.5];
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.5, epsilon);
   osc = LFOTestInjector::make(8.0, 2.0, 0.0);
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:1.0];
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 1.0, epsilon);
   osc = LFOTestInjector::make(8.0, 1.0, 0.0);
-  [self sample:osc.getNextValue() equals:0.0];
-  [self sample:osc.getNextValue() equals:0.5];
-  [self sample:osc.getNextValue() equals:1.0];
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.0, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 0.5, epsilon);
+  XCTAssertEqualWithAccuracy(osc.getNextValue(), 1.0, epsilon);
 }
 
 @end
