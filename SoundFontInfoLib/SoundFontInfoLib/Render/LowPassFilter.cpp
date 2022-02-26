c43ad73434b940ef9f884f83afb325a980068877
Brad Howes <bradhowes@mac.com>
2022-02-26 15:12:35 +0100

    New LowPassFilter

---
+++ SoundFontInfoLib/SoundFontInfoLib/Render/LowPassFilter.cpp
@@ -0,0 +1,35 @@
+// Copyright © 2020 Brad Howes. All rights reserved.
+
+#include "LowPassFilter.hpp"
+
+using namespace SF2::Render;
+
+enum Index { B0 = 0, B1, B2, A1, A2 };
+
+void
+LowPassFilter::update()
+{
+  auto cutoff = state_.modulated(State::Index::initialFilterCutoff);
+  auto resonance = state_.modulated(State::Index::initialFilterResonance);
+
+  const Float frequencyRads = DSP::PI * cutoff * nyquistPeriod_;
+  const Float r = ::pow(10.0f, 0.05f * -resonance);
+  const double k  = 0.5 * r * ::sin(frequencyRads);
+  const double c1 = (1.0 - k) / (1.0 + k);
+  const double c2 = (1.0 + c1) * ::cos(frequencyRads);
+  const double c3 = (1.0 + c1 - c2) * 0.25;
+
+  F_[B0] = c3;
+  F_[B1] = c3 + c3;
+  F_[B2] = c3;
+  F_[A1] = -c2;
+  F_[A2] = c1;
+
+  // As long as we have the same number of channels, we can use Accelerate's function to update the filter.
+  if (setup_ != nullptr) {
+    vDSP_biquadm_SetTargetsDouble(setup_, F_.data(), updateRate_, threshold_, 0, 0, 1, 1);
+  }
+  else {
+    setup_ = vDSP_biquadm_CreateSetup(F_.data(), 1, 1);
+  }
+}
---
+++ SoundFontInfoLib/SoundFontInfoLib/Render/LowPassFilter.hpp
@@ -0,0 +1,44 @@
+// Copyright © 2020 Brad Howes. All rights reserved.
+
+#pragma once
+
+#include <iostream>
+#include <Accelerate/../Frameworks/vecLib.framework/Headers/vForce.h>
+
+#include "DSP/DSP.hpp"
+#include "Entity/SampleHeader.hpp"
+#include "Render/LFO.hpp"
+#include "Render/State.hpp"
+
+namespace SF2::Render {
+
+class LowPassFilter
+{
+public:
+  LowPassFilter(const State& state) : state_{state}, nyquistPeriod_{1.0f / Float(0.5f * state.sampleRate())} {}
+
+  void update();
+
+  void apply(std::vector<Float*> const& ins, std::vector<Float*>& outs, size_t frameCount) const
+  {
+    assert(ins.size() == outs.size() && ins.size() == 1);
+    vDSP_biquadm(setup_, (float const**)ins.data(), vDSP_Stride(1), (float**)outs.data(), vDSP_Stride(1),
+                 vDSP_Length(frameCount));
+  }
+
+private:
+  
+  const State& state_;
+  Float nyquistPeriod_;
+
+  std::array<double, 5> F_{0.0};
+  vDSP_biquadm_Setup setup_{nullptr};
+
+  Float lastCutoff_{-1.0};
+  Float lastResonance_{1E10};
+
+  float threshold_{0.05f};
+  float updateRate_{0.4f};
+};
+
+} // namespace SF2::Render
