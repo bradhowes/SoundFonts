1893561c03ed53c7ec7d43fe87598a5c25eecd1b
Brad Howes <bradhowes@mac.com>
2022-04-16 16:11:11 -0400

    New SF2AU target

---
+++ SoundFontsApp/SoundFontsApp/SF2AU/DSP/Helpers/SF2AUDSPKernelAdapter.h
@@ -0,0 +1,30 @@
+//
+//  SF2SAUDSPKernelAdapter.h
+//  SF2SAU
+//
+//  Created by Brad Howes on 16/04/2022.
+//  Copyright © 2022 Brad Howes. All rights reserved.
+//
+
+#import <AudioToolbox/AudioToolbox.h>
+
+@class AudioUnitViewController;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SF2AUDSPKernelAdapter : NSObject
+
+@property (nonatomic) AUAudioFrameCount maximumFramesToRender;
+@property (nonatomic, readonly) AUAudioUnitBus *inputBus;
+@property (nonatomic, readonly) AUAudioUnitBus *outputBus;
+
+- (void)setParameter:(AUParameter *)parameter value:(AUValue)value;
+- (AUValue)valueForParameter:(AUParameter *)parameter;
+
+- (void)allocateRenderResources;
+- (void)deallocateRenderResources;
+- (AUInternalRenderBlock)internalRenderBlock;
+
+@end
+
+NS_ASSUME_NONNULL_END
