f8d176a4ec99b9033d8ab1f2b088c7e35e2ef8c1
Brad Howes <bradhowes@mac.com>
2022-04-16 16:11:11 -0400

    New SF2AU target

---
+++ SoundFontsApp/SoundFontsApp/SF2AU/SF2AU-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  SF2SAU-Bridging-Header.h
+//  SF2SAU
+//
+//  Created by Brad Howes on 16/04/2022.
+//  Copyright © 2022 Brad Howes. All rights reserved.
+//
+
+#import "SF2AUDSPKernelAdapter.h"
+#import "SF2AUAudioUnit.h"
