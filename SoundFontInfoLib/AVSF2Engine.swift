a3c3fe7d5aa51cb34f250eabd25a2988875f0f91
Brad Howes <bradhowes@mac.com>
2022-04-21 13:27:24 -0400

    Remove meaningless inheritance

--- SoundFontInfoLib/AVSF2Engine.swift
+++ SoundFontInfoLib/AVSF2Engine.swift
@@ -1,22 +1,17 @@
-//
-//  AVSF2Engine.swift
-//  SoundFontInfoLib
-//
-//  Created by Brad Howes on 17/04/2022.
-//  Copyright © 2022 Brad Howes. All rights reserved.
-//
+// Copyright © 2022 Brad Howes. All rights reserved.
 
 import AVFAudio
 import AUv3Support
 
-public final class AVSF2Engine: AVAudioUnitMIDIInstrument {
-
+/**
+ An enum masquerading as a namespace. Originally this was a class derived from `AVAudioUnitMIDIInstrument`, but there
+ is no way to instantiate a class like that from within the AV framework. Instead, the component description indicates
+ the `AV*` class one will get for an audio unit via the `componentType` attribute. For type `aumu` this will be an
+ `AVAudioUnitMIDIInstrument` instance, while for an audio effect (type `aufx`) this will be an `AVAudioUnit` instance.
+ */
+public enum AVSF2Engine {
   public static let audioComponentDescription = AudioComponentDescription(componentType: FourCharCode("aumu"),
                                                                           componentSubType: FourCharCode("sf2e"),
                                                                           componentManufacturer: FourCharCode("bray"),
                                                                           componentFlags: 0, componentFlagsMask: 0)
-
-  override public init() {
-    super.init(audioComponentDescription: Self.audioComponentDescription)
-  }
 }
