5867cacbfd4d75ec40499ec92d9a89cdbc2ca8f1
Brad Howes <bradhowes@mac.com>
2023-04-04 23:14:05 +0200

    Added more MIDI controller names.

--- SoundFontsFramework/SoundFontsFramework/Audio/MIDIControllerState.swift
+++ SoundFontsFramework/SoundFontsFramework/Audio/MIDIControllerState.swift
@@ -33,6 +33,30 @@ enum MIDICC: UInt8 {
   case softPedal = 67
   case legatoSwitch = 68
   case hold2 = 69
+  case soundController1 = 70
+  case soundController2 = 71
+  case soundController3 = 72
+  case soundController4 = 73
+  case soundController5 = 74
+  case soundController6 = 75
+  case soundController7 = 76
+  case soundController8 = 77
+  case soundController9 = 78
+  case soundController10 = 79
+  case portamentoAmount = 84
+  case effect1 = 91
+  case effect2 = 92
+  case effect3 = 93
+  case effect4 = 94
+  case effect5 = 95
+  case allSoundOff = 120
+  case resetAllControllers = 121
+  case localOnOff = 122
+  case allNotesOff = 123
+  case omniModeOff = 124
+  case omniModeOn = 125
+  case monoMode = 126
+  case polyMode = 127
 
   var name: String {
     switch self {
@@ -53,6 +77,30 @@ enum MIDICC: UInt8 {
     case .softPedal: return "Soft Pedal"
     case .legatoSwitch: return "Legato Switch"
     case .hold2: return "Hold 2"
+    case .soundController1: return "Sound Controller 1"
+    case .soundController2: return "Sound Controller 2"
+    case .soundController3: return "Sound Controller 3"
+    case .soundController4: return "Sound Controller 4"
+    case .soundController5: return "Sound Controller 5"
+    case .soundController6: return "Sound Controller 6"
+    case .soundController7: return "Sound Controller 7"
+    case .soundController8: return "Sound Controller 8"
+    case .soundController9: return "Sound Controller 9"
+    case .soundController10: return "Sound Controller 10"
+    case .portamentoAmount: return "Portamento Amount"
+    case .effect1: return "Effect 1 Depth"
+    case .effect2: return "Effect 2 Depth"
+    case .effect3: return "Effect 3 Depth"
+    case .effect4: return "Effect 4 Depth"
+    case .effect5: return "Effect 5 Depth"
+    case .allSoundOff: return "All Sound Off"
+    case .resetAllControllers: return "Reset All Controllers"
+    case .localOnOff: return "Local On/Off"
+    case .allNotesOff: return "All Notes Off"
+    case .omniModeOff: return "Omni Mode Off"
+    case .omniModeOn: return "Omni Mode On"
+    case .monoMode: return "Mono Mode"
+    case .polyMode: return "Poly Mode"
     }
   }
 }
